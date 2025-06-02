38723e5440d43621684b7fc885a4c7582f9780e4
OmegaOne <omegaone2025@outloot.com>
2025-06-03 08:28:57 +0900

    feat: initialize commit

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import js from '@eslint/js';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+
+dotenv.config();
 
 export default [
   js.configs.recommended,
